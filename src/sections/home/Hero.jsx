defa2282f482cf6a1450e3433074a5d504a47dda
FernandoTelloL <fernando_tellol@hotmail.com>
2024-06-08 00:11:31 -0500

    cambio de altura de hero home

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-    <section className='h-[70vh] flex flex-col items-center justify-center text-center'>
+    <section className='h-[80vh] flex flex-col items-center justify-center text-center'>
       <img src='./img/foto.png' className='w-[200px] h-[200px] object-cover rounded-full mb-12 border border-accent-dark p-1 bg-accent-dark/10 backdrop-blur-sm' alt='Fernando Tello' />
       <h1 className='text-3xl font-bold mb-4 sm:text-6xl text-textColorDark-primary'>FERNANDO TELLO</h1>
       <h2 className='text-2xl text-accent-dark sm:text-3xl'>Full Stack Developer</h2>
