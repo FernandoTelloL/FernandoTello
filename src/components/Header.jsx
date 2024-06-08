1119b359ba3c2db83c89fbe26bf4c0e4e074f911
FernandoTelloL <fernando_tellol@hotmail.com>
2024-06-07 23:56:19 -0500

    terminado estilos de navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,29 @@ export const Header = () => {
   return (
     <header className='container sticky top-0 z-50 sm:h-16 bg-red-500'>
       <div className='container mx-auto flex justify-end items-center p-4 transition-all h-full bg-primary-dark'>
-        <nav className='hidden md:flex space-x-4 transition-all'>
-          <a href='#inicio' className='hover:text-gray-400'>Inicio</a>
-          <a href='#nosotros' className='hover:text-gray-400'>Nosotros</a>
+        <nav className='hidden md:block'>
+          <ul className='md:flex space-x-4 transition-all'>
+            <li>
+              <a href='#inicio' className='hover:text-gray-400 md:mr-4'>Inicio</a>
+            </li>
+            <li>
+              <a href='#proyectos' className='hover:text-gray-400 md:mr-4'>Proyectos</a>
+            </li>
+            <li>
+              <a href='#experiencia' className='hover:text-gray-400 md:mr-4'>Experiencia</a>
+            </li>
+            <li>
+              <a href='#blog' className='hover:text-gray-400 md:mr-4'>Blog</a>
+            </li>
+            <li>
+              <a href='#contacto' className='hover:text-gray-400'>Contacto</a>
+            </li>
+          </ul>
         </nav>
         <div className='md:hidden'>
           <button onClick={toggleMenu} className='focus:outline-none'>
             <svg
-              className='w-6 h-6 text-accent-dark stroke-current'
+              className='w-9 h-9 text-accent-dark stroke-current '
               fill='none'
               viewBox='0 0 24 24'
               xmlns='http://www.w3.org/2000/svg'
@@ -28,9 +43,24 @@ export const Header = () => {
         </div>
       </div>
       {isMenuOpen && (
-        <div className='md:hidden absolute top-16 left-0 w-full bg-gray-800 p-4 transition-all'>
-          <a href='#inicio' className='block py-2 px-4 hover:bg-gray-700 rounded'>Inicio</a>
-          <a href='#nosotros' className='block py-2 px-4 hover:bg-gray-700 rounded'>Nosotros</a>
+        <div className='md:hidden absolute top-16 left-0 w-full bg-secondary-dark p-4 transition-all text-center rounded-lg text-lg'>
+          <ul>
+            <li>
+              <a href='#inicio' className='block py-2 px-4 hover:bg-gray-700 rounded mb-4'>Inicio</a>
+            </li>
+            <li>
+              <a href='#proyectos' className='block py-2 px-4 hover:bg-gray-700 rounded mb-4'>Proyectos</a>
+            </li>
+            <li>
+              <a href='#experiencia' className='block py-2 px-4 hover:bg-gray-700 rounded mb-4'>Experiencia</a>
+            </li>
+            <li>
+              <a href='#blog' className='block py-2 px-4 hover:bg-gray-700 rounded mb-4'>Blog</a>
+            </li>
+            <li>
+              <a href='#contacto' className='block py-2 px-4 hover:bg-gray-700 rounded mb-4'>Contacto</a>
+            </li>
+          </ul>
         </div>
       )}
     </header>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { Header } from './Header'
 export const Layout = ({ children }) => {
   return (
     <div className='bg-primary-dark text-textColorDark-primary font-inter'>
-      <div className='w-full mx-auto container lg:max-w-4xl md:max-w-2xl'>
+      <div className='w-full max-w-[85%] mx-auto container lg:max-w-4xl md:max-w-2xl'>
         <Header />
         <main className=''>
           {children}
