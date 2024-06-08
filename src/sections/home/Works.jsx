0cd6d282bb660cbb9c43ee225f64f505932f0d32
FernandoTelloL <fernando_tellol@hotmail.com>
2024-06-08 03:10:16 -0500

    agregado target_blank a links de proyectos

--- src/sections/home/Works.jsx
+++ src/sections/home/Works.jsx
@@ -1,6 +1,6 @@
 export const Works = () => {
   return (
-    <section id='experiencia' data-section='experiencia' className='section scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-2xl'>
+    <section id='experiencia' data-section='experiencia' className='section scroll-m-20 w-full mx-auto my-24 container lg:max-w-4xl md:max-w-2xl '>
       <h2 className='flex items-center mb-6 text-3xl font-semibold gap-x-3 '>
         <svg className='size-8' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z' /><path d='M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2' /><path d='M12 12l0 .01' /><path d='M3 13a20 20 0 0 0 18 0' />
         </svg>
