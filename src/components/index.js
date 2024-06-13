ea49954b785d5237b3a45ae3002064eaf828fcfb
FernandoTelloL <fernando_tellol@hotmail.com>
2024-06-13 03:39:15 -0500

    agregado estilos e iconos a habilidades

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-multiple-empty-lines */
 
+export { CssIcon, GitIcon, HtmlIcon, JavascriptIcon, MySQLIcon, PostgresIcon, ReactIcon, SkillsIcon, FirebaseIcon } from './Icons'
+
 export { DownloadButton } from './DownloadButton'
 
 export { Layout } from './Layout'
