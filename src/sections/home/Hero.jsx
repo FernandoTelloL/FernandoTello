1331ea60efa1a6ee6fffb1938128da49b6613b1c
Fernando Tello <fernando_tellol@hotmail.com>
2025-03-20 21:57:07 -0500

    actualizacion de subtitulo de seccion Hero

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,24 +1,38 @@
-import { DownloadButton } from '../../components'
+import { DownloadButton } from "../../components";
 
 export const Hero = () => {
   return (
-    <section id='inicio' className='h-[70vh] md:h-[80vh] flex flex-col items-center justify-center text-center'>
+    <section
+      id='inicio'
+      className='h-[70vh] md:h-[80vh] flex flex-col items-center justify-center text-center'
+    >
       <section className='mb-16'>
-        <img src='./img/foto.png' className='w-[200px] h-[200px] object-cover rounded-full mb-3 border border-accent-dark p-1 bg-accent-dark/10 backdrop-blur-sm' alt='Fernando Tello' />
+        <img
+          src='./img/foto.png'
+          className='w-[200px] h-[200px] object-cover rounded-full mb-3 border border-accent-dark p-1 bg-accent-dark/10 backdrop-blur-sm'
+          alt='Fernando Tello'
+        />
 
-        <p className='text-xs rounded-full border py-1 border-accent-dark bg-accent-dark/10'>Disponible para trabajar</p>
+        <p className='text-xs rounded-full border py-1 border-accent-dark bg-accent-dark/10'>
+          Disponible para trabajar
+        </p>
       </section>
       <section className='mb-5'>
-        <h1 className=' text-4xl md:text-5xl font-extrabold mb-1 text-textColorDark-primary'>FERNANDO TELLO</h1>
-        <h2 className='text-xl text-accent-dark sm:text-3xl'>Full Stack Developer</h2>
+        <h1 className=' text-4xl md:text-5xl font-extrabold mb-1 text-textColorDark-primary'>
+          FERNANDO TELLO
+        </h1>
+        <h2 className='text-xl text-accent-dark sm:text-3xl'>
+          FrontEnd Developer
+        </h2>
       </section>
 
       <section className='flex items-center justify-center w-full mt-1'>
         <DownloadButton />
 
-        <p className='text-xs border rounded-full px-4 py-2 leading-3 bg-accent-dark/10 border-accent-dark text-textColorDark-primary ml-4'> fernando_tellol@hotmail.com</p>
+        <p className='text-xs border rounded-full px-4 py-2 leading-3 bg-accent-dark/10 border-accent-dark text-textColorDark-primary ml-4'>
+          fernando_tellol@hotmail.com
+        </p>
       </section>
     </section>
-
-  )
-}
+  );
+};
